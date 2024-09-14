2badd9a740996b2759cea8db35e55be5af17b596
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2024-09-14 07:49:01 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import SearchBar from "./SearchBar"; // Import the SearchBar component
 function App() {
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center", marginTop: "50px" }}>Country Search</h1>
+      <h1
+        style={{
+          textAlign: "center",
+          marginTop: "50px",
+          letterSpacing: "5px",
+        }}
+      >
+        COUNTRY SEARCH
+      </h1>
       <SearchBar />
     </div>
   );
